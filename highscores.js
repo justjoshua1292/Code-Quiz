1c511ee0ad39c920dd5c781c97bd31ba1d13c1a6
Joshua Harris <Justjoshua1292@gmail.com>
2021-06-28 12:37:36 -0400

    Done

--- highscores.js
+++ highscores.js
@@ -3,6 +3,15 @@ var scoringol = document.getElementById("scoring");
 
 for (let index = 0; index < highscores.length; index++) {
 var newli = document.createElement("li")
-newli.textContent(highscores[index].initials+" - " + highscores[index].score)
+newli.textContent=highscores[index].initials+" - " + highscores[index].score
     scoringol.appendChild(newli)
 }
+
+function clearHighscores () {
+    localStorage.clear();
+    location.reload();
+}
+
+var clearButton = document.getElementById("clearBtn");
+
+clearButton.addEventListener("click", clearHighscores);
