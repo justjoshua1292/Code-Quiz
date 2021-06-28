9c7bae26f8a6784c03e097b080be869de7d2e8a5
Joshua Harris <Justjoshua1292@gmail.com>
2021-06-28 11:27:13 -0400

    adding changes to quiz, almost done

--- script.js
+++ script.js
@@ -66,3 +66,113 @@ function getQuestion() {
   });
 }
 
+function questionClick(answerChoice) {
+    // check if user guessed wrong  
+    if(answerChoice.textContent != questions[currentQuestionIndex].answer){
+      // penalize time
+      time -= 10;
+      // display new time on page
+      feedbackEl.textContent = "Incorrect";
+      // play "wrong" sound effect
+      sfxWrong.play();
+    }
+    // else 
+    else{
+      // play "right" sound effect
+      feedbackEl.textContent = "Correct";
+      sfxRight.play();
+    }
+  
+    // flash right/wrong feedback on page for half a second
+    feedbackEl.setAttribute("class", "feedback");
+    setInterval(function(){
+      feedbackEl.setAttribute("class", "feedback hide");
+    }, 500);
+  
+    // move to next question
+    currentQuestionIndex++;
+  
+    // check if we've run out of questions
+    if(currentQuestionIndex === questions.length)
+      // quizEnd
+      quizEnd();
+    // else 
+    else
+      // getQuestion
+      getQuestion();
+  }
+  
+  function quizEnd() {
+    // stop timer
+    clearInterval(timerId);
+    timerEl.textContent = time;
+  
+    // show end screen
+    var endScreenEl = document.getElementById("end-screen");
+    endScreenEl.setAttribute("class", " ");
+  
+    // show final score
+    var finalScoreEl = document.getElementById("final-score");
+    finalScoreEl.textContent = time;
+  
+    // hide questions section
+    questionsEl.setAttribute("class", "hide");
+  }
+  
+  function clockTick() {
+    // update time
+    time--;
+    timerEl.textContent = time;
+  
+    // check if user ran out of time
+    if(time <= 0)
+      quizEnd();
+    
+  }
+
+  function saveHighscore() {
+    // get value of input box
+    var initials = initialsEl.value.toUpperCase();
+    // make sure value wasn't empty
+    if(initials === ""){ 
+      alert("Input mustn't be blank'");
+      return;
+    }
+    else if(initials.length > 3){
+      alert("Input must be no more than 3 characters");
+      return;
+    }
+    else{
+      // get saved scores from localstorage, or if not any, set to empty array
+      var highscores;
+      if(JSON.parse(localStorage.getItem("highscores")) != null)
+        highscores = JSON.parse(window.localStorage.getItem("highscores"));
+      else
+        highscores = [];
+      // format new score object for current user
+      var newScore = {
+        initials: initials,
+        score: time
+      };
+      highscores.push(newScore);
+      // save to localstorage
+      localStorage.setItem("highscores", JSON.stringify(highscores));
+      // redirect to next page
+      location.href = "highscores.html";
+    }
+  }
+  
+  function checkForEnter(event) {
+    // check if event key is enter
+      // saveHighscore
+      if(event.keyCode === 13)
+        saveHighscore();
+  }
+  
+  // user clicks button to submit initials
+  submitBtn.onclick = saveHighscore;
+  
+  // user clicks button to start quiz
+  startBtn.onclick = startQuiz;
+  
+  initialsEl.onkeyup = checkForEnter;
